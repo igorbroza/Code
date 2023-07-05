86a6fb6bcbf8c3f36b127d43fca3b959e5b06a1b
igorbroza <igorbrozasantos127@gmail.com>
2023-07-05 01:11:45 -0300

    Home quase 100%

--- Arte_Gesso_WEB2/resources/views/home.blade.php
+++ Arte_Gesso_WEB2/resources/views/home.blade.php
@@ -6,53 +6,54 @@
         <title>Arte Gesso</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+
         <link href="{{ asset('css/carousel.css') }}" rel="stylesheet">
         <link href="{{ asset('css/about.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/especialidades.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
     </head>
 
     <body>
 
-    <header class="mb-7">
-        <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+    <header>
+        <div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <a class="navbar-brand" href="#">
+                            <img src="{{ asset('img/logoGesso.jpg') }}" alt="Avatar Logo" style="width:150px;" class="rounded-pill">
+                        </a>
+
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                            <div class="collapse navbar-collapse" id="navbarText" >
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Features</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Pricing</a>
+                                    </li>
+                                </ul>
+                            </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     </header>
 
-    <main>
+    <main class="bg-light">
+
         <div class="container-fluid">
             <div id="mainSlider" class="carousel slide" data-bs-ride="carousel">
 
@@ -90,16 +91,16 @@
                     </div>
                 </div>
             </div>
-        
+
 
 
             <a href="#mainSlider" class="carousel-control-prev" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+                <span class="sr-only"></span>
             </a>
             <a href="#mainSlider" class="carousel-control-next" role="button" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+                <span class="sr-only"></span>
             </a>
         </div>
 
@@ -119,17 +120,91 @@
                         <p>A equipe de profissionais especializados da Arte Gesso está preparada para atender às necessidades únicas de cada projeto</p>
                         <p>Uma das principais vantagens do Drywall é a rapidez na execução das obras. Em comparação aos métodos tradicionais de construção</p>
                         <ul id="about-list">
-                            <li><i class="fas fa-check"></i>Uma das principais vantagens do Drywall</li>
-                            <li><i class="fas fa-check"></i>Uma das principais vantagens do Drywall</li>
-                            <li><i class="fas fa-check"></i>Uma das principais vantagens do Drywall</li>
-                            <li><i class="fas fa-check"></i>Uma das principais vantagens do Drywall</li>
-                            <li><i class="fas fa-check"></i>Uma das principais vantagens do Drywall</li>
+                            <li>Uma das principais vantagens do Drywall</li>
+                            <li>Uma das principais vantagens do Drywall</li>
+                            <li>Uma das principais vantagens do Drywall</li>
+                            <li>Uma das principais vantagens do Drywall</li>
+                            <li>Uma das principais vantagens do Drywall</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="services-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="main-title">Nossas Especialidades Especialidades:</h3>
+                    </div>
+                    <div class="col-md-4 service-box">
+                        <img  src="{{ asset('img/gesso2.jpeg') }}" class="d-block w-100">
+                        <h4>Forros Decorado</h4>
+                        <p>Forro feito de drywal como otimo acabamento</p>
+                    </div>
+                    <div class="col-md-4 service-box">
+                        <img  src="{{ asset('img/gesso4.jpeg') }}" class="d-block w-100">
+                        <h4>Divisorias</h4>
+                        <p>Paredes feotas de drywall</p>
+                    </div>
+                    <div class="col-md-4 service-box">
+                        <img  src="{{ asset('img/gesso5.jpeg') }}" class="d-block w-100">
+                        <h4>Closets</h4>
+                        <p>Moveis resistentes e Bonitos</p>
+                    </div>
+                    <div class="col-md-4 service-box">
+                        <img  src="{{ asset('img/gesso2.jpeg') }}" class="d-block w-100">
+                        <h4>Nichos Decorados</h4>
+                        <p>Forro feito de drywal como otimo acabamento</p>
+                    </div>
+                    <div class="col-md-4 service-box">
+                        <img  src="{{ asset('img/gesso2.jpeg') }}" class="d-block w-100">
+                        <h4>Forro Mineral</h4>
+                        <p>Forro feito de drywal como otimo acabamento</p>
+                    </div>
+                    <div class="col-md-4 service-box">
+                        <img  src="{{ asset('img/gesso2.jpeg') }}" class="d-block w-100">
+                        <h4>Viga Falsa</h4>
+                        <p>Forro feito de drywal como otimo acabamento</p>
                     </div>
                 </div>
             </div>
         </div>
 
     </main>
+
+    <footer class="footer bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h5>Sobre:</h5>
+                    <p>Seu texto sobre a empresa ou site.</p>
+                </div>
+                <div class="col-lg-3">
+                    <h5>Links:</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Página Inicial</a></li>
+                        <li><a href="#">Sobre Nós</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h5>Contato:</h5>
+                    <ul class="list-unstyled">
+                        <li><i class="bi bi-geo-alt-fill"></i> Endereço da empresa</li>
+                        <li><i class="bi bi-envelope-fill"></i> exemplo@empresa.com</li>
+                        <li><i class="bi bi-telephone-fill"></i> (11) 1234-5678</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>&copy; 2023 Nome da Empresa. Todos os direitos reservados.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </body>
